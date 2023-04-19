b79045ca8a8239b1bb3bdd35bf29b2c2e0279044
Jeffrey McKinley <jeffrey@synapsestudios.com>
2023-04-19 15:04:31 -0700

    create po workflow

--- functional/src/PurchaseOrders/workflows/Create-PO.spec.ts
+++ functional/src/PurchaseOrders/workflows/Create-PO.spec.ts
@@ -12,7 +12,7 @@ describe("Create PO Workflow", () => {
 
   it("returns a uuid", async () => {
     const repo = constructPORepository();
-    const poResult = await createPO({ PORepo: repo })();
+    const poResult = await createPO({ PORepo: repo })([]);
     const id = poResult.unwrap();
     expect(poResult.isOk()).toBeTruthy();
     expect(isUuid(id)).toBeTruthy();
@@ -20,7 +20,7 @@ describe("Create PO Workflow", () => {
 
   it("saves a purchase order to a repository", async () => {
     const repo = constructPORepository();
-    const result = await createPO({ PORepo: repo })();
+    const result = await createPO({ PORepo: repo })([]);
     const id = result.unwrap();
     const poRes = await repo.fetch(id);
     const output = match(poRes, {
@@ -34,6 +34,32 @@ describe("Create PO Workflow", () => {
     expect(isPurchaseOrder(output)).toBeTruthy();
     if (isPurchaseOrder(output)) {
       expect(id).toEqual(output.id);
+    }
+  });
+
+  it("saves the line items passed into the initial purchase order creation to that po", async () => {
+    const repo = constructPORepository();
+    const plantPotOrder = {
+      itemNumber: '111111',
+      price: 4.99,
+      description: 'plant pot',
+      quantity: 4000
+    };
+    const result = await createPO({ PORepo: repo })([plantPotOrder]);
+    const id = result.unwrap();
+    const poRes = await repo.fetch(id);
+    const output = match(poRes, {
+      Ok: {
+        Some: (result) => result,
+        None: () => "No results for that search.",
+      },
+      Err: (err) => `Error: ${err}.`,
+    });
+
+    expect(isPurchaseOrder(output)).toBeTruthy();
+    if (isPurchaseOrder(output)) {
+      expect(id).toEqual(output.id);
+      expect(output.lineItems).toEqual(expect.arrayContaining([expect.objectContaining(plantPotOrder)]))
     }
   });
 });
--- functional/src/PurchaseOrders/workflows/Create-PO.ts
+++ functional/src/PurchaseOrders/workflows/Create-PO.ts
@@ -1,10 +1,32 @@
+import { flow } from 'lodash/fp'
 import { IPORepository } from "../domain/IPORepository";
-import { createPurchaseOrder } from "../domain/PurchaseOrder";
+import { PurchaseOrder, createPurchaseOrder, addLineItemToPO } from "../domain/PurchaseOrder";
+import { createItem } from '../domain/Item';
+
+type LineItemDTO = { itemNumber: string, price: number, description: string, quantity: number }[];
 
 export const createPO =
   ({ PORepo }: { PORepo: IPORepository }) =>
-  async () => {
-    const purchaseOrder = createPurchaseOrder();
+  async (lineItems: LineItemDTO) => {
+    const addLineItemsToPO = AddLineItemsToPO(lineItems);
+    const purchaseOrder = flow(
+        createPurchaseOrder,
+        addLineItemsToPO,
+    )();
     const res = await PORepo.save(purchaseOrder);
     return res.map(() => purchaseOrder.id);
   };
+
+const AddLineItemsToPO = (lineItems: LineItemDTO) => (purchaseOrder: PurchaseOrder) => addLineItemsToPOIterative({ purchaseOrder, lineItems}, 0)
+
+
+function addLineItemsToPOIterative(order: {purchaseOrder: PurchaseOrder, lineItems: LineItemDTO}, iterator: number): PurchaseOrder {
+  if (iterator === order.lineItems.length) return order.purchaseOrder;
+  if (!order.lineItems[iterator]) return order.purchaseOrder;
+
+  const { quantity, ...lineItem} = order.lineItems[iterator];
+  const item = createItem(lineItem.itemNumber, lineItem.price, lineItem.description);
+  const purchaseOrder = addLineItemToPO({ PO: order.purchaseOrder, item, quantity });
+
+  return addLineItemsToPOIterative({purchaseOrder, lineItems: order.lineItems}, iterator + 1);
+}
